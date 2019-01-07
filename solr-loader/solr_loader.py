b316da11b2261fa05214a206f72c3d41e51f9b72
Deepak Unni <deepak.unni3@gmail.com>
2019-01-07 10:12:20 -0800

    retab

--- solr-loader/solr_loader.py
+++ solr-loader/solr_loader.py
@@ -53,23 +53,23 @@ def load(docs):
     solr.add(docs)
 
 def verify_curie(curie):
-	"""
-	Verify a given CURIE
-	"""
-	curie_list = curie.split(':')
-	if len(curie_list) != 2:
-		if "_" in curie:
-			logging.warning("{} should actually be {}".format(curie,curie.replace('_', ':')))
-			curie_list = curie.split('_')
-		else:
-			logging.error("{} is not a proper CURIE")
-	return ':'.join(curie_list)
+    """
+    Verify a given CURIE
+    """
+    curie_list = curie.split(':')
+    if len(curie_list) != 2:
+        if "_" in curie:
+            logging.warning("{} should actually be {}".format(curie,curie.replace('_', ':')))
+            curie_list = curie.split('_')
+        else:
+            logging.error("{} is not a proper CURIE")
+    return ':'.join(curie_list)
 
 def get_prefix(curie):
-	"""
-	Extract prefix from a given CURIE
-	"""
-	return curie.split(':')[0]
+    """
+    Extract prefix from a given CURIE
+    """
+    return curie.split(':')[0]
 
 
 solr = None
@@ -77,63 +77,63 @@ DOCUMENT_CATEGORY = "annotation"
 SUBCLASS_OF = 'subClassOf'
 
 prefix_to_file_map = {
-	'OBA': 'oba.obo',
-	'NCIT': 'ncit.obo',
-	'HP': 'hp.obo',
-	'MONDO': 'mondo.obo',
-	'EFO': 'efo.obo'
+    'OBA': 'oba.obo',
+    'NCIT': 'ncit.obo',
+    'HP': 'hp.obo',
+    'MONDO': 'mondo.obo',
+    'EFO': 'efo.obo'
 }
 
 ontologies = {}
 documents = []
 
 if __name__ == "__main__":
-	args = parser.parse_args()
-	solr = pysolr.Solr(url = args.solr_url)
+    args = parser.parse_args()
+    solr = pysolr.Solr(url = args.solr_url)
 
-	with open(args.input, 'r') as FH:
-		for line in FH:
-			element = line.rstrip().split('\t')
-			ontology_id = element[-1]
-			ontology_id = verify_curie[ontology_id]
-			isa_closure = None
-			isa_closure_label = None
-			if ontology_id not in term_cache:
-				prefix = get_prefix(ontology_id)
-				ontology = None
-				if prefix in ontologies:
-					ontology = ontologies[prefix]
-				else:
-					ontology_factory = OntologyFactory()
-					logging.info("loading ontology: {}".format(prefix))
-					ontology = ontology_factory.create(prefix_to_file_map[prefix])
-					ontologies[prefix] = ontology
+    with open(args.input, 'r') as FH:
+        for line in FH:
+            element = line.rstrip().split('\t')
+            ontology_id = element[-1]
+            ontology_id = verify_curie[ontology_id]
+            isa_closure = None
+            isa_closure_label = None
+            if ontology_id not in term_cache:
+                prefix = get_prefix(ontology_id)
+                ontology = None
+                if prefix in ontologies:
+                    ontology = ontologies[prefix]
+                else:
+                    ontology_factory = OntologyFactory()
+                    logging.info("loading ontology: {}".format(prefix))
+                    ontology = ontology_factory.create(prefix_to_file_map[prefix])
+                    ontologies[prefix] = ontology
 
-				isa_closure = get_closure(ontology, ontology_id, [SUBCLASS_OF], True)
-				isa_closure_label = [ontology.label(x) for x in isa_closure]
-				term_cache[ontology_id] = {}
-				term_cache[ontology_id]['isa_closure'] = isa_closure
-				term_cache[ontology_id]['isa_closure_label'] = isa_closure_label
-			else:
-				isa_closure = term_cache[ontology_id]['isa_closure']
-				isa_closure_label = term_cache[ontology_id]['isa_closure_label']
+                isa_closure = get_closure(ontology, ontology_id, [SUBCLASS_OF], True)
+                isa_closure_label = [ontology.label(x) for x in isa_closure]
+                term_cache[ontology_id] = {}
+                term_cache[ontology_id]['isa_closure'] = isa_closure
+                term_cache[ontology_id]['isa_closure_label'] = isa_closure_label
+            else:
+                isa_closure = term_cache[ontology_id]['isa_closure']
+                isa_closure_label = term_cache[ontology_id]['isa_closure_label']
 
-			document = {
-				'id': hashlib.sha256(line.encode()).hexdigest(),
-				'document_category': DOCUMENT_CATEGORY,
-				'study_accession': element[2],
-				'variable_phv': element[0],
-				'tag_id': element[1],
-				'study_url': element[3],
-				'study_dataset_url': element[4],
-				'ontology_class': ontology_id,
-				'ontology_class_label': ontology.label(ontology_id),
-				'isa_closure': isa_closure,
-				'isa_closure_label': isa_closure_label
-			}
-			documents.append(document)
+            document = {
+                'id': hashlib.sha256(line.encode()).hexdigest(),
+                'document_category': DOCUMENT_CATEGORY,
+                'study_accession': element[2],
+                'variable_phv': element[0],
+                'tag_id': element[1],
+                'study_url': element[3],
+                'study_dataset_url': element[4],
+                'ontology_class': ontology_id,
+                'ontology_class_label': ontology.label(ontology_id),
+                'isa_closure': isa_closure,
+                'isa_closure_label': isa_closure_label
+            }
+            documents.append(document)
 
-	logging.debug(documents)
-	load(documents)
-	solr.optimize()
+    logging.debug(documents)
+    load(documents)
+    solr.optimize()
 
